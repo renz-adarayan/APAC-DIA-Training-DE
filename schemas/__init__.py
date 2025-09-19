07fad04c190bfe2856c4b1bcdcb09dc646e36c11
Renz Adarayan <Renz.Adarayan@insight.com>
2025-09-19 13:35:37 +0800

    Add initial schema imports and define __all__ for module exports

---
+++ schemas/__init__.py
@@ -0,0 +1,11 @@
+from .schemas import (
+    customers_schema, products_schema, stores_schema, suppliers_schema,
+    orders_header_schema, orders_lines_schema, events_schema, sensors_schema,
+    exchange_rates_schema, shipments_schema, returns_day1_schema,
+)
+
+__all__ = [
+    'customers_schema', 'products_schema', 'stores_schema', 'suppliers_schema',
+    'orders_header_schema', 'orders_lines_schema', 'events_schema', 'sensors_schema',
+    'exchange_rates_schema', 'shipments_schema', 'returns_day1_schema'
+]
