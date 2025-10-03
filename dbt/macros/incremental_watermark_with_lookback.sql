a16891539f12a94e1c7f789c37aa9c558f8ca185
Renz Adarayan <Renz.Adarayan@insight.com>
2025-10-03 14:13:40 +0800

    refactor: update timestamp calculation in incremental_watermark_with_lookback macro for consistency

--- dbt/macros/incremental_watermark_with_lookback.sql
+++ dbt/macros/incremental_watermark_with_lookback.sql
@@ -4,7 +4,7 @@
   {#- lookback_hours: number of hours to look back for late arrivals -#}
   {{ ts_col }} >= (
     select coalesce(
-      dateadd('hour', -{{ lookback_hours }}, max({{ ts_col }})), 
+      max({{ ts_col }}) - INTERVAL '{{ lookback_hours }} hours', 
       '1970-01-01'::timestamp
     ) 
     from {{ this }}
